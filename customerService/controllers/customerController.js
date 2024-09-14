d55c38cc72f12d875fcd7b92dff6c49a09bcaba5
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-09-14 18:36:19 +0530

    Customer Service Initialized

---
+++ customerService/controllers/customerController.js
@@ -0,0 +1,56 @@
+const Bill = require('../models/Customer');
+
+exports.getAllCustomers = async (req, res) => {
+    try {
+        const bills = await Customer.find();
+        res.status(200).json(customers);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.getCustomerById = async (req, res) => {
+    try {
+        const bill = await Customer.findById(req.params.id);
+        if (!customer) {
+            return res.status(404).json({ message: 'Customer not found' });
+        }
+        res.status(200).json(bill);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.createCustomer = async (req, res) => {
+    try {
+        const newCustomer = new Customer(req.body);
+        await newCustomer.save();
+        res.status(201).json(newCustomer);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+exports.updateCustomer = async (req, res) => {
+    try {
+        const customer = await newCustomer.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!customer) {
+            return res.status(404).json({ message: 'customer not found' });
+        }
+        res.status(200).json(customer);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+exports.deleteCustomer = async (req, res) => {
+    try {
+        const customer = await Customer.findByIdAndDelete(req.params.id);
+        if (!customer) {
+            return res.status(404).json({ message: 'Customer not found' });
+        }
+        res.status(200).json({ message: 'Customer deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
