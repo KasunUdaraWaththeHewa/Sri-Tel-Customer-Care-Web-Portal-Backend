4fc65c868eb6447fcb092f7013070ccf488ac2cb
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-09-14 10:41:06 +0530

    initial Auth Service added

---
+++ authService/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const PORT=process.env.PORT || 8070;
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true, //access-control-allow-credentials:true
+  };
+  
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+const URL=process.env.MONGODB_URL;
+mongoose.connect(URL,{
+    //useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    //useFindAndModify:false
+});
+const connection=mongoose.connection;
+connection.once("open",()=>{
+    console.log("MongoDB Connection Success!");
+})
+
+const userRoutes=require("./routes/user.js");
+app.use("/user",userRoutes);
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is up and running on port: ${PORT}`);
+})
