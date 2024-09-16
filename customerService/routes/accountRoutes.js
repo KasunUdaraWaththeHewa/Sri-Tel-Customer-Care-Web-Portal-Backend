71fb2314ac0221bdfa8e022d6d1a7cf3b574368c
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-09-16 12:22:56 +0530

    account model , controller and routes done for customerService

---
+++ customerService/routes/accountRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const accountController = require('../controllers/accountController');
+
+router.post('/', accountController.createAccount);
+router.put('/:id', accountController.updateAccount);
+router.post('/activate/:email', accountController.activateAccount);
+router.post('/deactivate/:email', accountController.deactivateAccount);
+router.post('/suspend/:email', accountController.suspendAccount);
+router.get('/customer/:email', accountController.getAllAccountsForCustomer);
+
+module.exports = router;
