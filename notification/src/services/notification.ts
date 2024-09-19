007bc71d8902de7c24bab95483fac80eb22a798d
nsavinda <nirmalsavinda29@gmail.com>
2024-09-19 17:34:37 +0530

    feat:notification producer

--- notification/src/services/notification.ts
+++ notification/src/services/notification.ts
@@ -12,11 +12,11 @@ const consumer: Consumer = kafka.consumer({ groupId: 'my-group' });
 interface EmailMessage {
   email: string;
   msg: string;
-  type: string;
+  title: string;
 }
 
 // Email sending function (using nodemailer for demonstration)
-const sendMail = async (email: string, message: string, type: string): Promise<void> => {
+const sendMail = async (email: string, message: string, title: string): Promise<void> => {
   const transporter: Transporter = nodemailer.createTransport({
     host: 'mail',
     port: 1025, // Default MailHog SMTP port
@@ -27,7 +27,7 @@ const sendMail = async (email: string, message: string, type: string): Promise<v
   const mailOptions: SendMailOptions = {
     from: 'your-email@gmail.com', // Use environment variable here
     to: email,
-    subject: `New Message - ${type}`,
+    subject: `Sri Tel - ${title}`,
     text: message
   };
 
@@ -42,15 +42,15 @@ const sendMail = async (email: string, message: string, type: string): Promise<v
 // Consumer function to subscribe to the topic and handle incoming messages
 const consumeMessages = async (): Promise<void> => {
   await consumer.connect();
-  await consumer.subscribe({ topic: 'test', fromBeginning: true });
+  await consumer.subscribe({ topic: 'notification', fromBeginning: true });
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }: EachMessagePayload) => {
       if (message.value) { // Check if value is not null
         const content: string = message.value.toString();
-        const { email, msg, type }: EmailMessage = JSON.parse(content);
-        console.log(`Received message: ${msg}, Email: ${email}, Type: ${type}`);
-        await sendMail(email, msg, type);
+        const { email, msg, title }: EmailMessage = JSON.parse(content);
+        console.log(`Received message: ${msg}, Email: ${email}, Title: ${title}`);
+        await sendMail(email, msg, title);
       } else {
         console.error(`Received null or undefined message value in topic: ${topic} at partition: ${partition}`);
       }
