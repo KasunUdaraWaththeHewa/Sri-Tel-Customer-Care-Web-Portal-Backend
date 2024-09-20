6b443bc0d6921f79747e917434df38605f026c86
Kariyakaranage Lahiru Sandaruwan <lahirusandaruwan501@gmail.com>
2024-09-20 22:51:55 +0530

    Refactor routes for ring tones and subscription-based services

--- valueAddedService/routes/ringToneRoutes.js
+++ valueAddedService/routes/ringToneRoutes.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const { personalizeTone } = require('../controllers/ringToneController');
+const express = require("express");
+const {
+  personalizeTone,
+  getAllActiveTones,
+} = require("../controllers/ringToneController");
 const router = express.Router();
 
-router.post('/personalize-tone', personalizeTone);
-router.get('/active-tones/:accountID', getAllActiveTones);
+router.post("/personalize-tone", personalizeTone);
+router.get("/active-tones/:accountID", getAllActiveTones);
 
 module.exports = router;
