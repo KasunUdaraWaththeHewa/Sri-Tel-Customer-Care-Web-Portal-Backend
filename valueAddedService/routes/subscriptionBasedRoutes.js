d5f780503edc1919312402d77d3c682e4d83a303
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-09-21 20:12:21 +0530

    active subscription and deactivate subscription put routes changed to post routes

--- valueAddedService/routes/subscriptionBasedRoutes.js
+++ valueAddedService/routes/subscriptionBasedRoutes.js
@@ -7,8 +7,8 @@ const {
 
 const router = express.Router();
 
-router.put("/activate-subscription", activateSubscription);
-router.put("/deactivate-subscription", deactivateSubscription);
+router.post("/activate-subscription", activateSubscription);
+router.post("/deactivate-subscription", deactivateSubscription);
 router.get("/active-subscriptions/:accountID", getAllActiveSubscriptions);
 
 module.exports = router;
