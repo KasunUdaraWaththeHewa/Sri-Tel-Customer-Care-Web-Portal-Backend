135f8e0a3a4d7a0c60ebbfe1326650b71ccaa348
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-09-21 21:07:45 +0530

    International Roaming modified

--- valueAddedService/routes/internationalRoamingRoutes.js
+++ valueAddedService/routes/internationalRoamingRoutes.js
@@ -6,8 +6,8 @@ const {
 } = require("../controllers/internationalRoamingController");
 const router = express.Router();
 
-router.put("/activate-roaming", activateRoaming);
-router.put("/deactivate-roaming", deactivateRoaming);
+router.post("/activate-roaming", activateRoaming);
+router.post("/deactivate-roaming", deactivateRoaming);
 router.get("/is-active-roaming/:accountID", isActiveRoaming);
 
 
